fa6737662c1b1fa3f2548e47a5e3a4e40c7df0bb
gabryelperfetti10 <163077907+gabryelperfetti10@users.noreply.github.com>
2024-10-23 21:52:25 -0300

    23102024

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+    let respostaEstaVisivel = false
+    function viraCartao(){
+        respostaestaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+        cartao.addEventListener('click', viraCartao)
+    }
+    cartao.className = 'cartao'container.appendChild(cartao)
 }
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+<h3>Programação</h3>
+<div class="cartao__conteudo__pergunta">
+        <p>O que é JavaScript?</p>
+</div>
+<div class="cartao__conteudo__resposta">
+        <p>O JavaScript é uma linguagem de programação</p>
+</div>
+</div>
+`
